5ecd0050f291279a364beaa778b5ee0538736a33
El Cuato <sstsacm@gmail.com>
2020-07-23 01:25:35 -0500

    Actualizacion

--- src/javaBasics/Herencias.java
+++ src/javaBasics/Herencias.java
@@ -1,5 +1,15 @@
 package javaBasics;
 
 public class Herencias {
-
+	
 }
+
+class HerenciasHijo{
+	void hola () {
+		
+	}
+	void adios() {
+		
+	}
+}
+
--- src/javaBasics/IniciarVariables.java
+++ src/javaBasics/IniciarVariables.java
@@ -1,19 +1,6 @@
 package javaBasics;
 
 public class IniciarVariables {
-	String base = "a";
-	
-	public IniciarVariables(){
-		base = appendS(base, "b");
-	}
-	{
-		base = appendS(base, "c");
-	}
-	
-	private String appendS(String base, String append) {
-		return base + append;
-	}
-	
 	
 	public static void main(String[] args) {
 		byte b = Byte.MAX_VALUE;
@@ -36,67 +23,7 @@ public class IniciarVariables {
 		byte b2 = (byte) s2;
 		char c2 = (char) b2;
 //		
-//		
-//		System.out.println("");
-////		
-//		Hijo h = new Hijo();
-//		Test t = h;
-//		h = (Hijo) t;
-//		System.out.println("ADIOS");
-//		String a;
-//		StringBuffer b;
-//		StringBuilder c;
-//		
-//		char algo = (char) 0;
-////		UtilEnum algo = UtilEnum.BLACK;
-//		
-//		
-//		
-//		switch (algo) {
-//		case 'a' | 'b' | (char) 100:
-//			System.out.println(1);
-////		case PINK : case GREEN:
-////			System.out.println("ROSA");
-////			break;
-////		case BLACK:
-////			System.out.println("NEGRO");
-//			break;
-//		default:
-//			break;
-//		}
-//		
-//		"".intern();
-		
-//		Integer.valueOf(12.0);
-//		System.out.println(algo());
-		
 	}
-	
-	static Object algo() {
-		return Integer.valueOf(1);
-	}
-	
-	String algo(char algo){
-		switch (algo) {
-		case 'a' | 'b' | (char) 100:
-			return "OTRA COSA";
-//		case PINK : case GREEN:
-//			System.out.println("ROSA");
-//			break;
-//		case BLACK:
-//			System.out.println("NEGRO");
-		default:
-			break;
-		}
-		return "";
-	}
+
 }
 
-class Hijo extends IniciarVariables{
-	void hola () {
-		
-	}
-	void adios() {
-		
-	}
-}
